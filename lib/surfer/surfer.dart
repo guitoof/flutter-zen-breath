43fb01b8e7d86af9bfaf68ff9036577d6d3ee37b
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2021-07-30 12:40:16 +0200

    :dizzy: (Surfer) Add sufer head tilt animation

--- lib/surfer/surfer.dart
+++ lib/surfer/surfer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart';
 
 class Surfer extends StatefulWidget {
   final Offset position;
@@ -7,7 +8,29 @@ class Surfer extends StatefulWidget {
   SurferState createState() => SurferState();
 }
 
-class SurferState extends State<Surfer> {
+class SurferState extends State<Surfer> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation _tiltAnimation;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(duration: const Duration(seconds: 1), vsync: this);
+
+    _tiltAnimation = IntTween(begin: 0, end: 20).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _animationController.repeat(reverse: true);
+        }
+      });
+
+    _animationController.forward();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
@@ -15,17 +38,21 @@ class SurferState extends State<Surfer> {
         width: 400,
         height: 400,
         child: Stack(
-          overflow: Overflow.visible,
+          clipBehavior: Clip.none,
           children: [
             Positioned(
               child:
                   Image.asset('assets/images/surfer/surfer_body.png', scale: 2),
             ),
             Positioned(
-              top: -110,
-              left: -10,
-              child: Image.asset('assets/images/surfer/surfer_head_louisz.png',
-                  scale: 2),
+              top: -103,
+              left: 0,
+              child: Transform.rotate(
+                angle: _tiltAnimation.value * degrees2Radians,
+                child: Image.asset(
+                    'assets/images/surfer/surfer_head_louisz.png',
+                    scale: 2),
+              ),
             )
           ],
         ),
